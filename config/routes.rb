aec0ab601d94368cfe3865331fd81da906ec9c18
Inderpreet Singh <inder@wastedcode.com>
2014-04-18 17:07:49 -0700

    Ability to navigate through the PRs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 GitReal::Application.routes.draw do
-  get 'auth',         to: 'users#auth'
-  get 'oauth/github', to: 'users#handle_auth'
-  get 'login',        to: 'users#login'
-  get 'logout',       to: 'users#logout'
+  get 'auth',             to: 'users#auth'
+  get 'oauth/github',     to: 'users#handle_auth'
+  get 'login',            to: 'users#login'
+  get 'logout',           to: 'users#logout'
+
+  # Repos
+  get 'repos/:repo_name', to: 'homepage#repos', as: 'repo_show'
 
   root to: 'homepage#index'
 end
