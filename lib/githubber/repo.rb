27c38c31ee26ecf574d53304e85fb83d31cb66b7
Inderpreet Singh <inder@wastedcode.com>
2014-04-20 10:58:27 -0700

    Calculate and display stats by user

--- lib/githubber/repo.rb
+++ lib/githubber/repo.rb
@@ -14,6 +14,44 @@ module Githubber
       prs
     end
 
+    def stats(handle, repo_name)
+      prs_by_user = {}
+      number_of_prs_by_user = {}
+      unique_commenters_by_user = {}
+
+      possible_repos(handle, repo_name).each do |h, r|
+        all_pull_requests(h, r).each do |pr|
+          user = pr.user.login
+          prs_by_user[user] = {} unless prs_by_user[user]
+
+          num_prs = number_of_prs_by_user[user] || 0
+          number_of_prs_by_user[user] = num_prs + 1
+
+          commenters = Set.new
+          comments_for_pr(pr.number, h, r).each do |comment|
+            commenting_user = comment.user.login
+            commenters.add(commenting_user)
+            existing_comments = prs_by_user[user][commenting_user] || 0
+            prs_by_user[user][commenting_user] = existing_comments + 1
+          end
+
+          commenters.each do |commenter|
+            unique_commenters = unique_commenters_by_user[user] || {}
+            previous_num = unique_commenters[commenter] || 0
+            unique_commenters[commenter] = previous_num + 1
+
+            unique_commenters_by_user[user] = unique_commenters
+          end
+        end
+      end
+
+      {
+        comments_for_user: prs_by_user,
+        number_of_prs_by_user: number_of_prs_by_user,
+        unique_commenters_by_user: unique_commenters_by_user
+      }
+    end
+
     def possible_repos(handle, repo_name)
       names = [[handle, repo_name]]
       parent = github_api.repos.find(handle, repo_name).body.parent
@@ -35,6 +73,19 @@ module Githubber
       end
 
       all_prs
+    end
+
+    def comments_for_pr(pr_number, handle, repo)
+      all_comments = []
+      github_api.pull_requests.comments.list(handle, repo, request_id: pr_number, per_page: 100).each_page do |page|
+        all_comments.concat(page)
+      end
+
+      github_api.issues.comments.list(handle, repo, issue_id: pr_number, per_page: 100).each_page do |page|
+        all_comments.concat(page)
+      end
+
+      all_comments
     end
   end
 end
